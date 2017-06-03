d3de1e6dd0517ed7262f4d6259806dc27d87f3aa
Felipe Hespanhol <felipeghespanhol@gmail.com>
2017-06-03 17:07:32 -0300

    validates condition names

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -19,4 +19,21 @@ class Condition < ApplicationRecord
   belongs_to :segment, inverse_of: :conditions
 
   validates :field, :name, :term, :segment, presence: true
+  validate :field_name_type
+
+  STRING_FIELD_ALLOWED_NAMES  = ['eq', 'cont', 'start', 'end']
+  INTEGER_FIELD_ALLOWED_NAMES = ['lt', 'lteq', 'gt', 'gteq']
+
+  private
+
+  def field_name_type
+    field_type = Contact.columns.find {|c| c.name == field}&.type
+    if field_type.nil?
+      errors.add(:field, "#{Contact.model_name.human} does not have field with name #{field}")
+    elsif field_type == :string && !STRING_FIELD_ALLOWED_NAMES.include?(name)
+      errors.add(:name, "must any of #{STRING_FIELD_ALLOWED_NAMES.join(', ')}")
+    elsif field_type == :integer && !INTEGER_FIELD_ALLOWED_NAMES.include?(name)
+      errors.add(:name, "must any of #{INTEGER_FIELD_ALLOWED_NAMES.join(', ')}")
+    end
+  end
 end
--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -22,4 +22,38 @@ RSpec.describe Condition, type: :model do
   it { is_expected.to validate_presence_of(:field) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:term) }
+
+  describe "#string_fields_names validation" do
+    let!(:segment) { create(:segment) }
+
+    ['name', 'email', 'state', 'job']. each do |contact_field|
+      context "when field is #{contact_field}" do
+        ['eq', 'cont', 'start', 'end'].each do |condition_name|
+          it "allows condition #{condition_name}" do
+            expect(segment.conditions.build(field: contact_field, name: condition_name, term: "teste")).to be_valid
+          end
+        end
+        ['lt', 'lteq', 'gt', 'gteq', 'other'].each do |condition_name|
+          it "does not allow condition #{condition_name}" do
+            expect(segment.conditions.build(field: contact_field, name: condition_name, term: "teste")).not_to be_valid
+          end
+        end
+      end
+    end
+
+    ['age']. each do |contact_field|
+      context "when field is #{contact_field}" do
+        ['lt', 'lteq', 'gt', 'gteq'].each do |condition_name|
+          it "allows condition #{condition_name}" do
+            expect(segment.conditions.build(field: contact_field, name: condition_name, term: "teste")).to be_valid
+          end
+        end
+        ['name', 'email', 'state', 'job', 'other'].each do |condition_name|
+          it "does not allow condition #{condition_name}" do
+            expect(segment.conditions.build(field: contact_field, name: condition_name, term: "teste")).not_to be_valid
+          end
+        end
+      end
+    end
+  end
 end
