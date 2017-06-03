a69ada0acc8e718a3db255867a70cc5b44c4115b
Felipe Hespanhol <felipeghespanhol@gmail.com>
2017-06-03 17:45:44 -0300

    added internationalization

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module RdChallenge
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :'pt-BR'
 
     config.generators do |g|
       g.test_framework       :rspec, view_specs: false, helper_specs: false
