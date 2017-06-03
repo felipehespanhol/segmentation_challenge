127ccfa92052a3eeccb7ae572703443da74cd845
Felipe Hespanhol <felipeghespanhol@gmail.com>
2017-06-03 20:43:22 -0300

    created segments#create

--- app/controllers/segments_controller.rb
+++ app/controllers/segments_controller.rb
@@ -5,5 +5,20 @@ class SegmentsController < ApplicationController
 
   def new
     @segment = Segment.new
+    @segment.conditions.build
+  end
+
+  def create
+    if @segment = Segment.create(segment_params)
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def segment_params
+    params.require(:segment).permit(:name, conditions_attributes: [:field, :name, :term])
   end
 end
--- spec/controllers/segments_controller_spec.rb
+++ spec/controllers/segments_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe SegmentsController, type: :controller do
+  render_views
+
   describe '#index' do
     let!(:segment1) { create(:segment) }
     let!(:segment2) { create(:segment) }
@@ -26,6 +28,49 @@ RSpec.describe SegmentsController, type: :controller do
 
     it 'returns http success' do
       expect(response).to be_success
+    end
+  end
+
+  describe "#create" do
+    context "when attributes are valid" do
+      let!(:segment_attributes) { 
+        {
+          segment: {
+            name: "Adultos São Paulo",
+            conditions_attributes: [
+              { field: 'state', name: 'eq', term: 'São Paulo' },
+              { field: 'age', name: 'gteq', term: '18' }
+            ]
+          }
+        }
+      }
+
+      it 'creates one segment' do
+        expect {
+          post :create, params: segment_attributes
+        }.to change {
+          Segment.count
+        }.by 1
+      end
+
+      it 'creates a segment with name' do
+        post :create, params: segment_attributes
+        segment = Segment.last
+        expect(segment.name).to eq(segment_attributes[:segment][:name])
+      end
+
+      it 'creates segment conditions' do
+        post :create, params: segment_attributes
+        segment = Segment.last
+        condition1 = segment.conditions[0]
+        condition2 = segment.conditions[1]
+        expect(condition1.field).to eq(segment_attributes[:segment][:conditions_attributes][0][:field])
+        expect(condition1.name).to eq(segment_attributes[:segment][:conditions_attributes][0][:name])
+        expect(condition1.term).to eq(segment_attributes[:segment][:conditions_attributes][0][:term])
+        expect(condition2.field).to eq(segment_attributes[:segment][:conditions_attributes][1][:field])
+        expect(condition2.name).to eq(segment_attributes[:segment][:conditions_attributes][1][:name])
+        expect(condition2.term).to eq(segment_attributes[:segment][:conditions_attributes][1][:term])
+      end
     end
   end
 end
